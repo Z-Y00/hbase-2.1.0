d6e4ed08cc6eebe4ab89b8770cef0c8157986cb2
GengYu Rao <zouyoo@outlook.com>
2018-09-30 02:02:46 +0800

    good, and fix testF

--- rbuild/testF.sh
+++ rbuild/testF.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+export LD_LIBRARY_PATH=/home/rdma_match/hbase-2.1.0/lib/native/Linux-amd64-64
+
 cd /home/rdma_match/hbase-2.1.0
 ./kill-region.sh
 ./clean.sh
@@ -11,6 +13,12 @@ create 'test', 'f', {SPLITS => (1..n_splits).map{|i| \"user#{1000+i*(9999-1000)/
 echo "done "
 
 cd /home/rdma_match/ycsb-hbase20-binding-0.15.0
+cd ./lib
+rm hbase-client-2.1.0.jar hbase-server-2.1.0.jar
+cp /home/rdma_match/hbase-2.1.0/lib/*.jar ./
+
+cd ..
+
 bin/ycsb load hbase20 -P workloads/workloadf -cp /home/rdma_match/hbase-2.1.0/conf -p table=test -p columnfamily=f
 
 bin/ycsb run hbase20 -P workloads/workloadf -cp /home/rdma_match/hbase-2.1.0/conf -p table=test -p columnfamily=f
