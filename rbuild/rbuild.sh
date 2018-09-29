1bc5429875f98142b238eb0a907d48b025aff56d
GengYu Rao <zouyoo@outlook.com>
2018-09-29 20:01:35 +0800

    fix conf bugs

--- rbuild/rbuild.sh
+++ rbuild/rbuild.sh
@@ -2,12 +2,16 @@
 ID='-i /home/rgy/.ssh/aliyun/id_rsa -P 25568  rdma_match@proxy.recolic.net'
 #ID=' -i ~/.ssh/aliyun/id_rsa  rdma_match@202.114.10.172'
 mkdir -p ./rbuild/bin/lib
-cd ./hbase-server/
-mvn package -DskipTests
-cp ./target/hbase-server-2.1.0.jar ../rbuild/bin/lib
-cd ../hbase-client/
+
+cd ./hbase-client/
 mvn package -DskipTests
 cp ./target/hbase-client-2.1.0.jar ../rbuild/bin/lib
+
+
+cd ../hbase-server/
+mvn package -DskipTests
+cp ./target/hbase-server-2.1.0.jar ../rbuild/bin/lib
+
 #find . -name 'hbase-server-2.1.0.jar'  -exec cp '{}' ./rbuild/bin/lib \;
 #find . -name 'hbase-client-2.1.0.jar'  -exec cp '{}' ./rbuild/bin/lib \;
 cd ../rbuild/bin/lib
